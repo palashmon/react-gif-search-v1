1d3f76ad41947bf05ce8126778b0d662c4927206
Palash Mondal <palashm@live.in>
2018-05-05 00:42:34 +0530

    Perform debounce on input text

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+const WAIT_INTERVAL = 1000;
+
 class SearchBar extends React.Component {
   state = { term: '' };
   static propTypes = {
     onTermChange: PropTypes.func.isRequired
   };
-  onInputChange(term) {
-    this.setState({ term });
-    this.props.onTermChange(term);
+
+  componentWillMount() {
+    this.timer = null;
   }
+
+  onInputChange(term) {
+    clearTimeout(this.timer);
+    this.setState({ term });
+    this.timer = setTimeout(this.triggerChange, WAIT_INTERVAL);
+  }
+
+  triggerChange = () => {
+    const { term } = this.state;
+    this.props.onTermChange(term);
+  };
 
   render() {
     return (
